7f82132eb433a863c363f84f21b678769be27004
alexbaluboby2016 <buitre2016balu@hotmail.com>
2024-11-13 10:55:01 +0100

    Create Drivers.bat

---
+++ Drivers.bat
@@ -0,0 +1,12 @@
+[Drivers]
+install usb 2.0
+install network 2.4
+install controller de video
+install plug-play 2.0
+install imgburn
+install Steam
+install Counter-Strike 2
+install desktop
+install EaseUS Partition Master
+install Revo Uninstaller Pro
+install µTorrent
